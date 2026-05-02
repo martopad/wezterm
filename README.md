d231c10ad5870a421d2d53948270fe336f782600
martopad <martin_luther_yung@outlook.com>
2026-05-02 11:46:03 +0200

    Add Windows specific config
    So that wezterm behavior is consistent
    on my Windows machine and linux machine

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # wezterm
 My wezterm configuration
+
+This configuration should be usable on both Windows and Linux.
+
+Requirements:
+1. Install Cascadia Mono
+    - For Windows:
+        - winget install fontget (assumes that you have winget installed)
+        - fontget add nerd.cascadiamono
--- wezterm.lua
+++ wezterm.lua
@@ -4,21 +4,35 @@ local wezterm = require 'wezterm'
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
-config.initial_cols = 180
-config.initial_rows = 40
-config.font = wezterm.font('Noto Sans Mono')
-config.font_size = 11
-
-config.enable_scroll_bar = true
-
--- Copy and slightly modify "Catppuccin Mocha" theme
--- Taken from https://github.com/catppuccin/WezTerm#-faq
-local custom = wezterm.color.get_builtin_schemes()["Catppuccin Mocha"]
+-- =====================================================================
+-- Theme
+-- Copy and slightly modify theme
+local custom = wezterm.color.get_builtin_schemes()["Light White (terminal.sexy)"]
+-- Set background to absolute black, for oled monitors
 custom.background = "#000000"
-config.color_schemes = {["OLEDppuccin"] = custom}
-config.color_scheme = "OLEDppuccin"
-
 -- White text is too bright. Replace it with slightly darker color.
 custom.foreground = "#b8b8b8"
+config.color_schemes = {["MyTheme"] = custom}
+config.color_scheme = "MyTheme"
+
+-- =====================================================================
+-- General settings
+config.enable_scroll_bar = true
+config.initial_cols = 180
+config.initial_rows = 40
+
+-- =====================================================================
+-- OS-specific settings
+if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
+    config.font = wezterm.font("Cascadia Mono", {weight="DemiBold", stretch="Normal", style="Normal"})
+    config.font_size = 9.0
+    config.default_prog = { 'wsl.exe', '--cd', '~' }
+    -- Match font renderring like Windows Terminal
+    config.freetype_load_target = 'Light'
+    config.freetype_render_target = 'HorizontalLcd'
+else
+    config.font = wezterm.font('Noto Sans Mono')
+    config.font_size = 11
+end
 
 return config
