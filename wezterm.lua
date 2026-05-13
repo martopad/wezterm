58c9b5b45b8ca37b42934313383a5c74bfdb5ac5
martopad <martin_luther_yung@outlook.com>
2026-05-13 15:05:55 +0200

    Add right click mapping

--- wezterm.lua
+++ wezterm.lua
@@ -3,6 +3,7 @@ local wezterm = require 'wezterm'
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
+local act = wezterm.action
 
 -- =====================================================================
 -- Theme
@@ -34,5 +35,24 @@ else
     config.font = wezterm.font("CaskaydiaCove Nerd Font Mono", {weight="Regular", stretch="Normal", style="Normal"})
     config.font_size = 11
 end
+
+
+-- =====================================================================
+-- Right click shortcut: When with selection, copy. When without selection, paste.
+config.mouse_bindings = {
+    {
+        event = { Down = { streak = 1, button = "Right" } },
+        mods = "NONE",
+        action = wezterm.action_callback(function(window, pane)
+            local has_selection = window:get_selection_text_for_pane(pane) ~= ""
+            if has_selection then
+                window:perform_action(act.CopyTo("ClipboardAndPrimarySelection"), pane)
+                window:perform_action(act.ClearSelection, pane)
+            else
+                window:perform_action(act({ PasteFrom = "Clipboard" }), pane)
+            end
+        end),
+    },
+}
 
 return config
